2c39db721d25ea1a497dfeb38a0dde0bce45a29c
Özgür Erol <erolxerolx@gmail.com>
2023-03-22 10:55:55 +0100

    the webpage now displays an error if the logging credentials are incorrect

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,35 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { LoginRequest } from 'src/app/interfaces/loginRequest';
+import { UserService } from 'src/app/services/user.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   myForm!: FormGroup;
   currentLoginStep: number = 0;
   showPasswordInput: boolean = false;
+  error: boolean = false;
+  buttonTexts = ['Continue', 'Login'];
+  currentButtonText = this.buttonTexts[0];
 
-
-  constructor(private fb: FormBuilder){}
+  constructor(private fb: FormBuilder, private userService: UserService) {}
 
   ngOnInit() {
     this.myForm = this.fb.group({
       email: '',
-      password: ''
-    })
+      password: '',
+    });
   }
 
   handleFirstLoginStep() {
     this.showPasswordInput = true;
     this.currentLoginStep++;
+    this.currentButtonText = this.buttonTexts[1];
   }
 
   handleLogin() {
     if (this.currentLoginStep == 0) {
       this.handleFirstLoginStep();
     } else if (this.currentLoginStep == 1) {
-      console.log('Logging in with email:', this.myForm.value.email, 'and password:', this.myForm.value.password);
+      let loginRequest: LoginRequest = {
+        email: this.myForm.value.email,
+        password: this.myForm.value.password,
+      };
+      
+      this.userService.sendLoginRequest(loginRequest).subscribe(
+        (data: any) => {
+          console.log('Logging in with email:', this.myForm.value.email, 'and password:', this.myForm.value.password);
+        },
+        (error: any) => {
+          this.error = true;
+        }
+      );
     }
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse, HttpErrorResponse} from '@angular/common/http';
+import { catchError, Observable, switchMap, throwError } from 'rxjs';
+import { of } from 'rxjs';
 import { User } from '../interfaces/user';
+import { LoginRequest } from '../interfaces/loginRequest';
 
 @Injectable({
   providedIn: 'root'
@@ -12,4 +15,25 @@ export class UserService {
   getUsers() {
     return this.http.get<User[]>('/api/user');
   }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => false);
+  }
+
+  sendLoginRequest(loginRequest: LoginRequest): Observable<any> {
+    return this.http.post("/api/user/login", loginRequest).pipe(
+      catchError(this.handleError)
+    );
+  }
+
 }
