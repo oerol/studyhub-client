c7fac4597818e542c8b13093e39638867ddca0fd
Özgür Erol <erolxerolx@gmail.com>
2023-03-22 16:57:48 +0100

    added custom html titles to the routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
+const titleAppendix = " @studyhub"
+
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: '', component: HomeComponent, title: "home" + titleAppendix },
+  { path: 'login', component: LoginComponent, title: "login" + titleAppendix },
+  { path: 'dashboard', component: DashboardComponent, title: "dashboard" + titleAppendix }
 ];
 
 @NgModule({
