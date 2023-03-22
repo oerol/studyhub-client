48d5afe50eab2a92b030d583663601cf694a98a5
Özgür Erol <erolxerolx@gmail.com>
2023-03-22 09:19:39 +0100

    created a basic login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,16 +7,29 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   myForm!: FormGroup;
+  currentLoginStep: number = 0;
+  showPasswordInput: boolean = false;
+
 
   constructor(private fb: FormBuilder){}
 
   ngOnInit() {
     this.myForm = this.fb.group({
       email: '',
+      password: ''
     })
   }
 
-  login() {
-    console.log('Logging in with email:', this.myForm.value.email, 'and password:', "this.password");
+  handleFirstLoginStep() {
+    this.showPasswordInput = true;
+    this.currentLoginStep++;
+  }
+
+  handleLogin() {
+    if (this.currentLoginStep == 0) {
+      this.handleFirstLoginStep();
+    } else if (this.currentLoginStep == 1) {
+      console.log('Logging in with email:', this.myForm.value.email, 'and password:', this.myForm.value.password);
+    }
   }
 }
