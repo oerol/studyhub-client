7e81d4ba05cd73bbefec3a658fbf89ec31d84e9c
Özgür Erol <erolxerolx@gmail.com>
2023-03-23 10:01:18 +0100

    improved the code for the login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -12,11 +12,14 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   myForm!: FormGroup;
   currentLoginStep: number = 0;
+
   showPasswordInput: boolean = false;
-  showEmailErrors: boolean = false;
-  error: boolean = false;
-  buttonTexts = ['Continue', 'Login'];
   @ViewChild('passwordInput', { static: false }) passwordInput!: ElementRef<HTMLInputElement>;
+
+  showEmailErrors: boolean = false;
+  authentificationError: boolean = false;
+
+  buttonTexts = ['Continue', 'Login'];
   currentButtonText = this.buttonTexts[0];
 
   constructor(private fb: FormBuilder, private userService: UserService, private router: Router, private changeDetectorRef: ChangeDetectorRef) {}
@@ -48,27 +51,41 @@ export class LoginComponent implements OnInit {
     }
   }
 
+  handleSecondLoginStep() {
+    const userEnteredPassword = this.passwordInput.nativeElement.value !== "";
+    if (userEnteredPassword) {
+      this.handleLoginRequest();
+    }
+  }
+
+  handleAuthentificationError() {
+    this.authentificationError = true;
+    this.passwordInput.nativeElement.value = "";
+  }
+
   handleLogin() {
+    console.log('Logging in with email:', this.myForm.value.email, 'and password:', this.myForm.value.password);
+    this.router.navigate(['/dashboard']);
+  }
+
+  handleLoginRequest() {
+    const loginRequest: LoginRequest = {
+      email: this.myForm.value.email,
+      password: this.myForm.value.password,
+    };
+    
+    // prettier-ignore
+    this.userService.sendLoginRequest(loginRequest).subscribe(
+      (success: any) => {this.handleLogin()},
+      (error: any) => {this.handleAuthentificationError()}
+    );
+  }
+
+  handleLoginAttempt() {
     if (this.currentLoginStep == 0) {
       this.handleFirstLoginStep();
     } else if (this.currentLoginStep == 1) {
-      if (this.passwordInput.nativeElement.value !== "") {
-        let loginRequest: LoginRequest = {
-          email: this.myForm.value.email,
-          password: this.myForm.value.password,
-        };
-  
-        this.userService.sendLoginRequest(loginRequest).subscribe(
-          (data: any) => {
-            console.log('Logging in with email:', this.myForm.value.email, 'and password:', this.myForm.value.password);
-            this.router.navigate(['/dashboard']);
-          },
-          (error: any) => {
-            this.error = true;
-            this.passwordInput.nativeElement.value = "";
-          }
-        );
-      }
+      this.handleSecondLoginStep();
     }
   }
 }
