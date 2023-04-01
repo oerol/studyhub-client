89c0a61253c5dfa551f660067a806d02f69be395
Özgür Erol <erolxerolx@gmail.com>
2023-04-01 19:27:23 +0200

    created a basic register form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['../shared-login.register.scss'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  myForm!: FormGroup;
 
+  constructor(private router: Router, private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: '',
+    });
+  }
+
+  toLogin() {
+    this.router.navigate(['/login']);
+  }
 }
