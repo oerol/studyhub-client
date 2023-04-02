a8eae25f3ce3ed69eb6fb80d73f4706b08fe685a
Özgür Erol <erolxerolx@gmail.com>
2023-04-02 12:06:59 +0200

    added password validation to the register form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators, ValidatorFn } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,15 +9,71 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
   myForm!: FormGroup;
+  showPasswordError: boolean = false;
+  showConfirmPasswordError: boolean = false;
 
-  constructor(private router: Router, private fb: FormBuilder) {}
+  @ViewChild('requiredminlength') requireMinLength!: ElementRef;
+  @ViewChild('requiredlowercase') requireLowerCase!: ElementRef;
+  @ViewChild('requireduppercase') requireUpperCase!: ElementRef;
+  @ViewChild('requirednumber') requireNumber!: ElementRef;
+
+  constructor(private router: Router, private fb: FormBuilder, private changeDetectorRef: ChangeDetectorRef) {}
 
   ngOnInit() {
     this.myForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
-      password: '',
+      password: [''],
+      confirmPassword: ['']
+    }, {validators: this.checkPasswords});
+
+
+
+    this.myForm.get('password')!.valueChanges.subscribe(() => {
+      const passwordControl = this.myForm.get('password');
+      const passwordValue: string = passwordControl!['value'];
+      this.showPasswordError = true;
+      this.changeDetectorRef.detectChanges();
+
+      if (passwordValue.length < 8) {
+        this.requireMinLength.nativeElement.style.color = 'red';
+      } else {
+        this.requireMinLength.nativeElement.style.color = 'green';
+      }
+
+      if (/[a-z]/.test(passwordValue)) {
+        this.requireLowerCase.nativeElement.style.color = 'green';
+      } else {
+        this.requireLowerCase.nativeElement.style.color = 'red';
+      }
+      if (/[A-Z]/.test(passwordValue)) {
+        this.requireUpperCase.nativeElement.style.color = 'green';
+      } else {
+        this.requireUpperCase.nativeElement.style.color = 'red';
+      }
+      if (/[0-9]/.test(passwordValue)) {
+        this.requireNumber.nativeElement.style.color = 'green';
+      } else {
+        this.requireNumber.nativeElement.style.color = 'red';
+      }
     });
   }
+  
+  // https://stackoverflow.com/a/51606362/14615037
+  checkPasswords: ValidatorFn = (group: AbstractControl):  ValidationErrors | null => { 
+    let password = group.get('password')!.value;
+    let confirmPassword = group.get('confirmPassword')!.value
+    return password === confirmPassword ? null : { notSame: true }
+  }
+
+  onConfirmPasswordInput() {
+    this.showConfirmPasswordError = true;
+  }
+
+  handleRegisterAttempt() {
+  }
+  
 
   toLogin() {
     this.router.navigate(['/login']);
