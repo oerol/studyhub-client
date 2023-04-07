65974633d340fafae50eeec8a19d562ac2b70190
Özgür Erol <erolxerolx@gmail.com>
2023-04-07 09:57:47 +0200

    added a view switcher for the course overview

--- src/app/components/courses-overview/courses-overview.component.ts
+++ src/app/components/courses-overview/courses-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-courses-overview',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./courses-overview.component.scss']
 })
 export class CoursesOverviewComponent {
+  constructor(private elementRef: ElementRef) {}
+
+  setActiveButton(button: HTMLButtonElement): void {
+    const buttons = this.elementRef.nativeElement.querySelectorAll('button');
+    console.log(this.elementRef.nativeElement);
+    
+    buttons.forEach((btn: HTMLButtonElement) => {
+      btn.classList.remove('active');
+    });
+    button.classList.add('active');
+  }
 
 }
