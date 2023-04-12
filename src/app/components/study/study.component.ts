35829175449e671105873463391d35c9d21ac78d
Özgür Erol <erolxerolx@gmail.com>
2023-04-12 11:04:27 +0200

    added a simple study button to the study page

--- src/app/components/study/study.component.ts
+++ src/app/components/study/study.component.ts
@@ -8,18 +8,15 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   animations: [
     trigger('fadeIn', [
       state('void', style({ opacity: 0 })),
-      transition(':enter', [
-        animate('300ms ease-out', style({ opacity: 1 }))
-      ]),
-      transition(':leave', [
-        animate('300ms ease-out', style({ opacity: 0 }))
-      ])
-    ])
-  ]
+      transition(':enter', [animate('300ms ease-out', style({ opacity: 1 }))]),
+      transition(':leave', [animate('300ms ease-out', style({ opacity: 0 }))]),
+    ]),
+  ],
 })
 export class StudyComponent {
-  pomodoroBreak: boolean = true;
+  pomodoroBreak: boolean = false;
   exitPomodoro() {
     this.pomodoroBreak = false;
   }
+  startStudying() {}
 }
